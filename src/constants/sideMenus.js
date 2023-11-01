cc952123615cfe743870de36a53df7f2d73b7e9c
udongmandu <qjawns10000@naver.com>
2023-11-01 17:06:07 +0900

    사이드 바 페이지 관리 체계 수정

---
+++ src/constants/sideMenus.js
@@ -0,0 +1,28 @@
+export const sideMenus = [
+  {
+    title: "학생관리",
+    menus: [
+      {
+        name: "회원관리",
+        href: "/",
+      },
+      {
+        name: "응애관리",
+        href: "/product",
+      },
+    ],
+  },
+  {
+    title: "돈관리",
+    menus: [
+      {
+        name: "회원관리2",
+        href: "/aa",
+      },
+      {
+        name: "응애관리2",
+        href: "/bb",
+      },
+    ],
+  },
+];
