5bde652d04c440001aebff1317f9d1ddc180b105
udongmandu <qjawns10000@naver.com>
2023-11-03 17:48:11 +0900

    Button 컴포넌트 기능 추가

--- src/Components/ButtonTop.jsx
+++ src/Components/ButtonTop.jsx
@@ -1,11 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 export default function Button(props) {
-  const { styleClass, onClick, disabled, label } = props;
+  const {
+    styleClass,
+    onClick,
+    disabled,
+    label,
+    URL,
+    width,
+    height,
+    fontColor,
+    bgColor,
+  } = props;
+
+  let cssStyle = "text-xs fontA h-10 px-2 rounded-md border ";
+
+  const dynamicStyle = {
+    width: width ? `${width}px` : "4rem",
+    height: height ? `${height}px` : "2.5rem",
+    color: fontColor ? `#${fontColor}` : "white",
+    backgroundColor: bgColor ? `#${bgColor}` : "#5272F2",
+  };
 
   return (
-    <button className={styleClass} onClick={onClick} disabled={disabled}>
-      <span className="font-extrabold fontA">{label}</span>
-    </button>
+    <Link to={URL}>
+      <button
+        style={styleClass ? {} : dynamicStyle}
+        className={styleClass ? styleClass : cssStyle}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {label}
+      </button>
+    </Link>
   );
 }
+
+Button.propTypes = {
+  URL: PropTypes.string,
+  onClick: PropTypes.func,
+  label: PropTypes.string,
+  styleClass: PropTypes.string,
+  disabled: PropTypes.bool,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fontColor: PropTypes.string,
+  bgColor: PropTypes.string,
+};
