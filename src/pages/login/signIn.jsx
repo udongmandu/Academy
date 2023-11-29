9f4293f1912bafa288132825eb9682582fb7c1d3
udongmandu <qjawns10000@naver.com>
2023-11-29 17:51:41 +0900

    로그인 기능 추가 및 topbar user이름 보이도록 설정

--- src/pages/login/signIn.jsx
+++ src/pages/login/signIn.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import Button from "../../Components/ButtonTop";
+import PropTypes from "prop-types";
 
-export default function LoginPage() {
+export default function LoginPage(props) {
+  const { login, user } = props;
+  console.log(user);
+
+  function loging(e) {
+    e.preventDefault();
+    const inputID = e.target.elements.ID.value;
+    // const inputPW = e.target.elements.PASSWORD.value;
+
+    //로컬 스토리지에 정보 로그인 정보 저장
+    const userData = { name: inputID, author: "admin" };
+    localStorage.setItem("user", JSON.stringify(userData));
+
+    window.location.href = "/";
+  }
+
   return (
     <>
       <div className="absolute w-[100vw] h-[100vh] top-0 left-0 bg-black opacity-50" />
@@ -16,7 +32,7 @@ export default function LoginPage() {
               bgColor={"5272F2"}
             ></Button>
           </div>
-          <form action="" className="flex justify-center">
+          <form onSubmit={(e) => loging(e)} className="flex justify-center">
             <div className="flex flex-col">
               <input
                 className="px-2 w-80 h-11 rounded-md border border-[#000000] my-7"
@@ -29,14 +45,14 @@ export default function LoginPage() {
                 placeholder={`비밀번호`}
               />
               <div className="flex justify-evenly pt-6">
+                <button className="text-xs w-20 h-10 px-2 rounded-md border bg-[#5272F2] text-white">
+                  로그인
+                </button>
                 <Button
                   label={"신규등록"}
                   width={80}
                   URL={"/register-page"}
                 ></Button>
-                <button className="text-xs w-20 h-10 px-2 rounded-md border bg-[#5272F2] text-white">
-                  로그인
-                </button>
               </div>
               <span className="text-xs font-extrabold pt-3 text-center">
                 신규등록을 통해 새로운 교사의 계정 추가 가능
--- src/template/Topbar.jsx
+++ src/template/Topbar.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/index.css";
 import Button from "../Components/ButtonTop";
 
 export default function Topbar() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  function logout() {
+    localStorage.removeItem("user");
+    window.location.reload();
+  }
+
   return (
     <div className="w-full h-24 flex items-center justify-between px-16 bg-[#F3F4F6] border-b-2 text-slate-500">
       <img className="w-48 h-12" src="" alt="" />
-      <Button
-        URL="/sign-in"
-        label="로그인"
-        styleClass={"w-28 h-11 border-[3px] rounded-lg fontA text-ms"}
-      />
+      {!user ? (
+        <Button
+          URL="/sign-in"
+          label="로그인"
+          styleClass={"w-28 h-11 border-[3px] rounded-lg fontA text-ms"}
+        />
+      ) : (
+        <div className="flex items-center gap-5">
+          <div className="font-extrabold text-lg">{user.name}</div>
+          <Button onClick={logout} label={"로그아웃"} width={80} />
+        </div>
+      )}
     </div>
   );
 }
