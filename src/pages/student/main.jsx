34174e50fd67fc0a3f072027c6b1c36c22c05a47
udongmandu <qjawns10000@naver.com>
2023-11-29 17:52:05 +0900

    main page 수정사항 반영

--- src/pages/student/main.jsx
+++ src/pages/student/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import DataTableV1 from "../../Components/dataTableV1/DataTableV1";
 import SearchBox from "../../Components/searchBox/SearchBox";
-import { sideMenus } from "../../constants/sideMenus";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import BasicBox from "../../Components/manage-box/BasicBox";
 
 export default function MainPage() {
   const [search, setSearch] = useState({
@@ -13,17 +12,6 @@ export default function MainPage() {
     endDate: "",
   });
   console.log(search);
-  //페이지 타이틀과 페이지 명 가져오기
-  const location = useLocation();
-  const targetPath = location.pathname;
-  let foundMenu = null;
-  sideMenus.forEach((menuGroup) => {
-    menuGroup.menus.forEach((menu) => {
-      if (menu.href === targetPath) {
-        foundMenu = { title: menuGroup.title, name: menu.name };
-      }
-    });
-  });
 
   const columns = [
     { columnName: "no", data: "no" },
@@ -39,31 +27,39 @@ export default function MainPage() {
     { name: "hello" },
     { name: "hello" },
     { name: "hello" },
+    { name: "hello", hi: "hi" },
+    { name: "hellasdfasfsdfo" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello", hi: "hi" },
+    { name: "hellasdfasfsdfo" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello", hi: "hi" },
+    { name: "hellasdfasfsdfo" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello", hi: "hi" },
+    { name: "hellasdfasfsdfo" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello", hi: "hi" },
+    { name: "hellasdfasfsdfo" },
+    { name: "hello" },
+    { name: "hello" },
+    { name: "hello" },
   ];
 
   return (
     <>
-      <div className="fontA text-3xl pt-10 pl-10">
-        {foundMenu ? (
-          <>
-            {foundMenu.title}{" "}
-            <span className="text-lg fontA">
-              {" "}
-              {">"} {foundMenu.name}
-            </span>
-          </>
-        ) : null}
-      </div>
-      <div className="w-full justify-center flex px-10 py-12">
-        <div className="min-w-[45vw] max-w-[800px] border-4 rounded-md shadow-2xl border-[#5272F2] px-10">
-          <SearchBox onSubmit={setSearch} option={"student"}></SearchBox>
-          <DataTableV1
-            title={"학생관리 테이블"}
-            columns={columns}
-            datas={data}
-          />
-        </div>
-      </div>
+      <BasicBox>
+        <SearchBox onSubmit={setSearch} option={"student"}></SearchBox>
+        <DataTableV1 title={"학생관리 테이블"} columns={columns} datas={data} />
+      </BasicBox>
     </>
   );
 }
